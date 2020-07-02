d189843a93ca8143a1d770f73130f5ee27630761
Gregory Casamento <greg.casamento@gmail.com>
2020-07-02 17:28:52 -0400

    Initial Commit

---
+++ NSTabView_test/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  NSTabView_test
+//
+//  Created by Gregory Casamento on 7/2/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
